f142002a01b95fcbd0e4fc105e61a4c44c01819f
digia <digia@DESKTOP-ENG9I36>
2017-06-11 17:34:59 +0200

    DefaultLoginService.java: minor changes to adapt class for testing LoginServiceTest.java: implemented test methods

--- src/main/java/org/dhis/security/DefaultLoginService.java
+++ src/main/java/org/dhis/security/DefaultLoginService.java
@@ -10,33 +10,33 @@ import com.github.benmanes.caffeine.cache.LoadingCache;
 public class DefaultLoginService
     implements LoginService
 {
-	private static final Integer MAX_ATTEMPTS = 5;
-	private static final Integer TIME_LIMIT_MINUTES = 60;
+	public static final Integer MAX_ATTEMPTS = 5;
+	public static final Integer EXPIRE_TIME = 60;
+	
     /**
      * Cache for login attempts where usernames are keys and login attempts are values.
      */
-    private final LoadingCache<String, Integer> USERNAME_LOGIN_ATTEMPTS_CACHE = Caffeine.newBuilder().expireAfterWrite(TIME_LIMIT_MINUTES, TimeUnit.MINUTES).build(key -> 0);
-    
-    // TODO Instantiate and configure this cache (https://github.com/ben-manes/caffeine)
-    
-    @Override
+    private final LoadingCache<String, Integer> USERNAME_LOGIN_ATTEMPTS_CACHE = Caffeine.newBuilder().expireAfterWrite(EXPIRE_TIME, TimeUnit.MINUTES).build(key -> 0);
+	
+	public LoadingCache<String, Integer> getUSERNAME_LOGIN_ATTEMPTS_CACHE() {
+		return USERNAME_LOGIN_ATTEMPTS_CACHE;
+	}
+
+	@Override
     public void registerAuthenticationFailure( AuthenticationEvent event )
     {
-        // TODO Implement this method        
     	USERNAME_LOGIN_ATTEMPTS_CACHE.put(event.getUsername(), USERNAME_LOGIN_ATTEMPTS_CACHE.get(event.getUsername()) + 1);
     }
 
     @Override
     public void registerAuthenticationSuccess( AuthenticationEvent event )
     {
-        // TODO Implement this method        
     	USERNAME_LOGIN_ATTEMPTS_CACHE.invalidate(event.getUsername());
     }
 
     @Override
     public boolean isBlocked( User user )
     {
-        // TODO Implement this method        
         return USERNAME_LOGIN_ATTEMPTS_CACHE.get(user.getUsername()) >= MAX_ATTEMPTS ? true : false;
     }
 }
--- src/test/java/org/dhis/security/LoginServiceTest.java
+++ src/test/java/org/dhis/security/LoginServiceTest.java
@@ -1,13 +1,16 @@
 package org.dhis.security;
 
-import org.dhis.security.DefaultLoginService;
-import org.dhis.security.LoginService;
+import static org.junit.Assert.*;
+
+import org.dhis.user.User;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.github.benmanes.caffeine.cache.LoadingCache;
+
 public class LoginServiceTest
 {
-    private LoginService loginService;
+    private DefaultLoginService loginService;
     
     @Before
     public void before()
@@ -16,8 +19,121 @@ public class LoginServiceTest
     }
     
     @Test
-    public void testFoo()
-    {
-        // TODO Implement at least two unit tests verifying the LoginService interface behavior
+    public void testRegisterAuthenticationSuccess_inCache() {
+//    	Prepare
+    	LoadingCache<String,Integer> cache = loginService.getUSERNAME_LOGIN_ATTEMPTS_CACHE();
+    	cache.put("foo", DefaultLoginService.MAX_ATTEMPTS - 1);
+    	AuthenticationEvent successfulEvent = new AuthenticationEvent("foo");
+    	
+//    	Execute
+    	loginService.registerAuthenticationSuccess(successfulEvent);
+    	
+//    	Verify
+    	Integer cacheAfterSuccess = cache.get("foo");
+    	assertEquals(0, cacheAfterSuccess.intValue());
+    }
+    
+    @Test
+    public void testRegisterAuthenticationSuccess_notInCache() {
+//    	Prepare
+    	LoadingCache<String,Integer> cache = loginService.getUSERNAME_LOGIN_ATTEMPTS_CACHE();
+    	cache.cleanUp();
+    	assertEquals(0, cache.estimatedSize());
+    	AuthenticationEvent successfulEvent = new AuthenticationEvent("foo");
+    	
+//    	Execute
+    	loginService.registerAuthenticationSuccess(successfulEvent);
+    	
+//    	Verify
+    	Integer cacheAfterSuccess = cache.get("foo");
+    	assertEquals(0, cacheAfterSuccess.intValue());
+    }
+    
+    @Test
+    public void testRegisterAuthenticationFailure_inCache() {
+//    	Prepare
+    	LoadingCache<String,Integer> cache = loginService.getUSERNAME_LOGIN_ATTEMPTS_CACHE();
+    	cache.put("foo", DefaultLoginService.MAX_ATTEMPTS - 1);
+    	AuthenticationEvent failingEvent = new AuthenticationEvent("foo");
+    	
+//    	Execute
+    	loginService.registerAuthenticationFailure(failingEvent);
+    	
+//    	Verify
+    	Integer cacheAfterFailure = cache.get("foo");
+    	assertEquals(DefaultLoginService.MAX_ATTEMPTS.intValue(), cacheAfterFailure.intValue());
+    }
+
+    @Test
+    public void testRegisterAuthenticationFailure_notInCache() {
+//    	Prepare
+    	LoadingCache<String,Integer> cache = loginService.getUSERNAME_LOGIN_ATTEMPTS_CACHE();
+    	cache.cleanUp();
+    	assertEquals(0, cache.estimatedSize());
+    	AuthenticationEvent failingEvent = new AuthenticationEvent("foo");
+    	
+//    	Execute
+    	loginService.registerAuthenticationFailure(failingEvent);
+    	
+//    	Verify
+    	Integer cacheAfterFailure = cache.get("foo");
+    	assertEquals(1, cacheAfterFailure.intValue());
+    }
+    
+    @Test
+    public void testIsBlocked_inCache_underAttemptLimit() {
+//    	Prepare
+    	LoadingCache<String,Integer> cache = loginService.getUSERNAME_LOGIN_ATTEMPTS_CACHE();
+    	cache.put("foo", DefaultLoginService.MAX_ATTEMPTS - 1);
+    	User user = new User("foo");
+    	
+//    	Execute
+    	boolean unblockedUser = loginService.isBlocked(user);
+    	
+//    	Verify
+    	assertEquals(false, unblockedUser);
+    }
+    
+    @Test
+    public void testIsBlocked_inCache_equalsAttemptLimit() {
+//    	Prepare
+    	LoadingCache<String,Integer> cache = loginService.getUSERNAME_LOGIN_ATTEMPTS_CACHE();
+    	cache.put("foo", DefaultLoginService.MAX_ATTEMPTS);
+    	User user = new User("foo");
+    	
+//    	Execute
+    	boolean blockedUser = loginService.isBlocked(user);
+    	
+//    	Verify
+    	assertEquals(true, blockedUser);
+    }
+    
+    @Test
+    public void testIsBlocked_inCache_overAttemptLimit() {
+//    	Prepare
+    	LoadingCache<String,Integer> cache = loginService.getUSERNAME_LOGIN_ATTEMPTS_CACHE();
+    	cache.put("foo", DefaultLoginService.MAX_ATTEMPTS + 1);
+    	User user = new User("foo");
+    	
+//    	Execute
+    	boolean blockedUser = loginService.isBlocked(user);
+    	
+//    	Verify
+    	assertEquals(true, blockedUser);
+    }
+    
+    @Test
+    public void testIsBlocked_notInCache() {
+//    	Prepare
+    	LoadingCache<String,Integer> cache = loginService.getUSERNAME_LOGIN_ATTEMPTS_CACHE();
+    	cache.cleanUp();
+    	assertEquals(0, cache.estimatedSize());
+    	User user = new User("foo");
+    	
+//    	Execute
+    	boolean blockedUser = loginService.isBlocked(user);
+    	
+//    	Verify
+    	assertEquals(false, blockedUser);
     }
 }
